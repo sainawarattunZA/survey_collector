8dad31c60faff4a8b5b8dae0eca3a0780460c710
Sai Nawarat Tun <150899545+sainawarattunZA@users.noreply.github.com>
2023-12-09 13:28:38 +0630

    hide ed

--- app/Filament/Pages/EditForm.php
+++ app/Filament/Pages/EditForm.php
@@ -9,4 +9,9 @@ class EditForm extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.edit-form';
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return false;
+    }
 }
