d33b1090b810d1cc515dd081528a432f00c2a71d
Sai Nawarat Tun <150899545+sainawarattunZA@users.noreply.github.com>
2023-12-11 15:08:23 +0630

    check

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -12,7 +12,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class DepartmentResource extends Resource
 {
